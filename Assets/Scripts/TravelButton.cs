b54a63c39040815b301d971dcdcb0acaae376313
Riuttasu <239182073+Riuttasu@users.noreply.github.com>
2026-02-21 05:14:17 +0100

    Package Dialogos y Travel poco

---
+++ Assets/Scripts/TravelButton.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TravelButton : MonoBehaviour
+{
+    public void TravelToScene()
+    {
+        string SceneLoaded = gameObject.name;
+
+        var manager = Object.FindFirstObjectByType<LevelManager>();
+        if (manager != null)
+        {
+            manager.LoadScene(SceneLoaded);
+        }
+    }
+}
