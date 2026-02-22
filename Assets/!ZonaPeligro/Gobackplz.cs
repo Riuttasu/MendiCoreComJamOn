934c8607d8125cc867905e85b4916671b3d08b49
Riuttasu <239182073+Riuttasu@users.noreply.github.com>
2026-02-22 18:52:04 +0100

    Porsi

---
+++ Assets/!ZonaPeligro/Gobackplz.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gobackplz : MonoBehaviour
+{
+    public void goback()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
